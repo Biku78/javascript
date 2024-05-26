1fec1e584116dc1de0c433205fa173f444e3ed71
Biku78 <bikramkesh.swain@gmail.com>
2024-05-26 12:39:04 +0000

    operation

--- variables/conversiontypeof.js
+++ variables/conversiontypeof.js
@@ -1,8 +1,21 @@
 let score=69
-console.log(score);
-console.log(typeof score)
-console.log(typeof"69");
+// console.log(score);
+// console.log(typeof score)
+// console.log(typeof"69");
 //convert string to number
 let string=String(score)
-console.log(string)
-console.log(typeof string);
+// console.log(string)
+// console.log(typeof string);
+//..............operation...............................
+let num1=2
+let num2=4
+let num=num1+num2
+//console.log(num);
+console.log(1+2);
+console.log(1+"2");
+console.log("1"+2+3)
+console.log(1+2+"3");
+console.log(1+2+"3+4+5");
+console.log(1+2+"3"+4+5);
+//****************logic******8 */
+console.log(+true);
