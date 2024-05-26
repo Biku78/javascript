b54424d7364a6e943c48f129c12b3a6ab5c16bcf
Biku78 <bikramkesh.swain@gmail.com>
2024-05-26 03:38:43 +0000

    javascrift variables

---
+++ variables/var.js
@@ -0,0 +1,7 @@
+const accountNumber=3456;
+let accountEmail="bikram@gmail.com"
+var AcountName="saving"
+// accountNumber="123"
+accountEmail="hello@gmail.com"
+accountName="current"
+console.table([accountNumber,accountEmail,accountName])
