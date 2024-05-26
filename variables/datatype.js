ce2bf30cb5cb2800af584da4d435ae5e3c64d702
Biku78 <bikramkesh.swain@gmail.com>
2024-05-26 04:16:45 +0000

    datatypes

---
+++ variables/datatype.js
@@ -0,0 +1,18 @@
+"use strict"//use js code to newly vesrsion
+
+let name="bikram"
+let age=18
+let isLoggedIn=true
+console.log(isLoggedIn);
+
+//number=2 to the power 53
+//string
+//null= objects
+//typeof
+//boolean= either true or false
+//symbol=undefined/
+//undefined
+
+console.log(typeof"bikram")
+console.log(typeof"null")
+console.log(typeof null)
