1943b90b45b61dd5fa71f0a4a87405144b878a5c
Biku78 <bikramkesh.swain@gmail.com>
2024-05-26 04:36:29 +0000

    typeof conversion

---
+++ variables/conversiontypeof.js
@@ -0,0 +1,8 @@
+let score=69
+console.log(score);
+console.log(typeof score)
+console.log(typeof"69");
+//convert string to number
+let string=String(score)
+console.log(string)
+console.log(typeof string);
