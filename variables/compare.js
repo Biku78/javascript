b034423932e3f53584f3ce8908fb80a20751b6ce
Biku78 <bikramkesh.swain@gmail.com>
2024-05-27 02:59:42 +0000

    comparesion in javascript

---
+++ variables/compare.js
@@ -0,0 +1,9 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 > 1);
+//these are simple comparesion as a math display output as a 
+console.log(null >=0);
+console.log(undefined>=0);
