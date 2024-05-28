14b52e8097d113747de4000dc14b22099b70e892
Biku78 <bikramkesh.swain@gmail.com>
2024-05-28 17:50:22 +0000

    comparesion

--- variables/compare.js
+++ variables/compare.js
@@ -7,3 +7,18 @@
 //these are simple comparesion as a math display output as a 
 console.log(null >=0);
 console.log(undefined>=0);
+console.log(undefined>=-0);
+//
+// Reference (non primitive )
+// array, objects,function
+//primitive
+// 7 types=string,number,boolean,null,,undefined,symbols,bigint 
+
+// ****************//************* */
+// stack memory(primitive) and non primitive (heap)
+let myYoutube="@swain"
+
+
+let anotherName=myYoutube
+// console.log(anotherName);
+
---
+++ variables/logic.js
@@ -0,0 +1,9 @@
+let objects={
+    name:"bikram",
+    email:"user@gmail.com"
+}
+let objects2=objects
+objects2.email="bikram@gmail.com"
+
+console.log(objects.email);
+console.log(objects2.email);
