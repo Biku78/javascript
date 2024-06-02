483a94fe5c47c2364e031c7c42fc664ac2ad43fc
Biku78 <bikramkesh.swain@gmail.com>
2024-06-02 09:30:07 +0530

    play with number and math

---
+++ variables/numandmath.js
@@ -0,0 +1,19 @@
+const score=400
+ console.log( score);
+
+ const balance=new Number(400)
+ console.log(balance);
+  console.log(balance.toString().length);
+  console.log(balance.toFixed(2));
+
+
+  const onum=23.785654
+  console.log(onum.toPrecision(4));
+
+  const num=100000
+  console.log(num.toLocaleString('en-IN'));
+
+//   ?**************** math*****************
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+// math.random()*(max-min+1) + min
