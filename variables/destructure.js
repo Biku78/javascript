ed5b6ed95bf215e5c3a182ff8be176d7861204ea
Biku78 <bikramkesh.swain@gmail.com>
2024-08-04 05:00:55 +0530

    destructure ans json

---
+++ variables/destructure.js
@@ -0,0 +1,28 @@
+const course= {
+    coursename:'hindi',
+    price:"123",
+    creator:"bikram"
+
+
+}
+//course.coursename[]
+// const {coursename}= course(right)
+const {coursename:bikram}= course//(right)
+
+console.log(bikram);
+
+//..............API................
+
+  // JSON API
+//   {
+//     "name":"bikram",
+//     "price":"123",
+//     "creator":"bikram"
+//   }
+
+
+    [
+        {},
+        {},
+        {}
+    ]
