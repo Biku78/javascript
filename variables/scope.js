e492db6a1183160185905ce47c59dd3739ee70a9
Biku78 <bikramkesh.swain@gmail.com>
2024-08-12 07:28:05 +0530

    using global and local scope

---
+++ variables/scope.js
@@ -0,0 +1,49 @@
+if(true){
+    const a=10
+    let b=20
+    var c=30
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// nested scope.........
+function one(){
+    const username="bikram"
+    function two(){
+        const website='youtube'
+        console.log(username);
+        console.log(website);
+        
+        
+    }
+    two()
+
+}
+one()
+
+//  ///////////////// nested if 
+
+if (true){
+    const username="bikram"
+    if(username==="bikram"){
+        const website=" youtube"
+        console.log(username+website);
+        
+    }
+}
+
+
+// intrresting facts
+
+addone(4)
+function addone(num){
+    return num+1
+}
+
+
+addtwo(5)
+const addtwo=function(num){
+    return num+2
+}
+
